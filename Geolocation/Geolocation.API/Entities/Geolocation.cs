225b6c24251c98e54beee8cd34b96b4aa4678904
Natalia Kowalik <natalia.kowalik@banqsoft.pl>
2022-06-11 15:12:30 +0200

    Added entities

---
+++ Geolocation/Geolocation.API/Entities/Geolocation.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Geolocation.Api.Entities
+{
+    public class Geolocation
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string Address { get; set; }
+
+        public GeolocationDetails GeoDetails { get; set; } = new GeolocationDetails();
+
+        public Geolocation(string address)
+        {
+            Address = address;
+        }
+    }
+}
---
+++ Geolocation/Geolocation.API/Entities/GeolocationDetails.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Geolocation.Api.Entities
+{
+    public class GeolocationDetails
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string? Ip { get; set; }
+        public string? Type { get; set; }
+        public string? ContinentCode { get; set; }
+        public string? Continent { get; set; }
+        public string? CountryCode { get; set; }
+        public string? Country { get; set; }
+        public string? RegionCode { get; set; }
+        public string? Region { get; set; }
+        public string? City { get; set; }
+        public string? Zip { get; set; }
+
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+
+        public string? Location { get; set; }
+
+        [ForeignKey("GeolocationId")]
+        public Geolocation? Geolocation { get; set; }
+        public int GeolocationId { get; set; }
+    }
+}
